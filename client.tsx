e9d548af3ac87de85a1745a91a0ab51cd0f32d52
Brandon Miller <brandon@oneilSoftware.com>
2024-06-21 10:39:51 -0700

    fix: Remove async keyword from App function

--- client.tsx
+++ client.tsx
@@ -2,7 +2,7 @@
 import { Text, View, Linking } from "react-native";
 
 ////////////BROKEN CODE BELOW without use client/////////////////////
-export default async function App() {
+export default function App() {
   return (
     <View  >
       <Text >Welcome to Expo + Next.js REACT NATIVE</Text>
