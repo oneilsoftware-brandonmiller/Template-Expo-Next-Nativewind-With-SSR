a75c17b2402dd977f049577a48a0bae428557107
brandon miller <brandon.miller@oneilsoftware.com>
2024-09-04 10:28:44 -0700

    updated to nativewind next

--- components/client.tsx
+++ components/client.tsx
@@ -4,8 +4,10 @@ import { Text, View } from "react-native";
 export default function App() {
   return (
     <View  >
-            <div className="w-10 h-10 bg-red-500" />
+      <div className="w-10 h-10 bg-red-500" />
       <Text className="font-bold">Welcome to Expo + Next.js REACT NATIVE</Text>
+      <Text className="font-bold">Welcome to Expo + Next.js REACT NATIVE</Text>
+
     </View>
   );
 }
